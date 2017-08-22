3a5341eda0c9acaee524b4b633d6200f1ebf2253
andriy-gazin <andriy.gazin@gmail.com>
2017-08-22 11:22:50 +0300

    add mean plot, rename charts and scripts

--- meanageybyregion&sex.R
+++ meanageybyregion&sex.R
@@ -1,31 +1,31 @@
 library(ggplot2)
 
 df <- read.csv('agebyregion&sex.csv', stringsAsFactors = F)
-df$region <- forcats::fct_reorder2(df$region, x = df$medianm, y = df$medianf)
+df$region <- forcats::fct_reorder2(df$region, x = -df$meanm, y = df$meanf)
 
-png('agebyregion&sex.png', width = 1000, height = 1000)
+png('meanagebyregion&sex.png', width = 1000, height = 1000)
 
 ggplot(df, aes(x = region))+
-  geom_linerange(aes(ymin = medianf, ymax = medianm), size = 9, color = '#e0e0e0')+
+  geom_linerange(aes(ymin = meanf, ymax = meanm), size = 9, color = '#e0e0e0')+
   geom_hline(yintercept = seq(23, 31, 1), size = 1.5, color = '#EFF2F4')+
-  geom_point(aes(y = medianf, fill = 'female'), alpha = 0.7,
+  geom_point(aes(y = meanf, fill = 'female'), alpha = 0.7,
              size = 9, shape = 21, color = '#5D646F', stroke = 1.5)+
-  geom_point(aes(y = medianm, fill = 'male'), alpha = 0.7,
+  geom_point(aes(y = meanm, fill = 'male'), alpha = 0.7,
              size = 9, shape = 21, color = '#5D646F', stroke = 1.5)+
-  geom_text(aes(y = medianf - 0.25, label = region, family = 'Ubuntu Condensed'), 
+  geom_text(aes(y = meanf - 0.25, label = region, family = 'Ubuntu Condensed'), 
             size = 6, color = '#3A3F4A', hjust = 1)+
-  scale_y_continuous(position = 'top', limits = c(21.25, 31),
-                     breaks = seq(23, 31, 1), 
-                     labels = c('23', '24', '25', rep('', 6)),
+  scale_y_continuous(position = 'top', limits = c(22, 29.5),
+                     breaks = seq(23, 29, 1), 
+                     labels = c('23', '24', '25', '26', rep('', 3)),
                      expand = c(0.025, 0.01), 
                      sec.axis = dup_axis(
-                       labels = c('', '', '25', '26', '27', '28', '29', '30', '31')))+
+                       labels = c('', '', '', '26', '27', '28', '29')))+
   scale_fill_manual(values = c('female' = '#b2182b', 'male' = '#2166ac'),
                     labels = c(' жінки ', ' чоловіки'))+
   
   coord_flip()+
   guides(fill = guide_legend(override.aes = list(alpha = 0.5)))+
-  labs(title = 'Медіанний вік першого шлюбу',
+  labs(title = 'Середній вік першого шлюбу',
        subtitle = 'В розрізі регіонів та статей.',
        caption = 'Дані: Державна служба статистики, 2015 рік | Візуалізація: Textura.in.ua')+
   theme_minimal()+
