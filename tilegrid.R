7f1dcddaf86ca7a8508490d42630a7f937f9c215
andriy-gazin <andriy.gazin@gmail.com>
2017-08-22 14:06:40 +0300

    add tilegrid maps

---
+++ tilegrid.R
@@ -0,0 +1,55 @@
+library(dplyr)
+library(ggplot2)
+
+df <- read.csv('agebyregion&sex.csv', stringsAsFactors = F)
+
+ukraine <- data.frame(
+  
+  row = c(1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5),
+  
+  col = c(2, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 2, 5, 6, 7, 4, 6, 5),
+  
+  code = c(1:27),
+  
+  name = as.character(c("Волинська", "Київ", "Чернігівська", "Сумська", "Львівська", "Тернопільська", "Рівненська", "Житомирська", "Київська", "Полтавська", "Харківська", "Луганська", "Закарпатська", "Івано-Франківська", "Хмельницька", "Вінницька", "Черкаська", "Кіровоградська", "Дніпропетровська", "Донецька", "Чернівецька", "Миколаївська", "Херсонська", "Запорізька", "Одеська", "АР Крим", "м. Севастополь")),
+  
+  stringsAsFactors = F
+  
+)
+
+ukraine <- ukraine %>% 
+  inner_join(df, by = c('name' = 'region')) %>% 
+  select(-code, -meanf, -meanm) %>% 
+  tidyr::gather(sex, age, -name, -row, -col) %>% 
+  mutate(sex = forcats::fct_recode(sex, чоловіки = 'medianm', жінки = 'medianf'))
+
+png('tilegrid_median.png', width = 1000, height = 1000)
+
+ggplot(ukraine)+
+  geom_tile(aes(x = col, y = row, fill = age), color = '#5D646F', size = 0.1)+
+  geom_text(aes(x = col, y = row, 
+                label = paste(name, age, sep = '\n'), family = 'Ubuntu Condensed'), 
+            color = '#3A3F4A', size = 4.5)+
+  scale_y_reverse()+
+  scale_fill_distiller(palette = 4, direction = 1)+
+  facet_wrap(~sex, ncol = 1)+
+  guides(fill = guide_colorbar(nbin = 10, raster = F, ticks = T,
+                                title = 'вік, років',
+                                title.position = 'top'))+
+  labs(title = 'Медіанний вік першого шлюбу',
+       subtitle = 'В розрізі регіонів та статей',
+       caption = 'Дані: Державна служба статистики, 2015 рік | Візуалізація: Textura.in.ua')+
+  theme_void()+
+  theme(text = element_text(family = 'Ubuntu Condensed', face = 'plain', color = '#3A3F4A'),
+        legend.position = 'none',
+        legend.key.height = unit(7.5, "pt"),
+        legend.key.width = unit(175, "pt"),
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(size = 18, face = 'bold'),
+        plot.title = element_text(face = 'bold', size = 44, margin = margin(b = 20, t = 10)),
+        plot.subtitle = element_text(size = 19, face = 'plain', margin = margin(b = 20)),
+        plot.caption = element_text(size = 16, margin = margin(b = 20, t = 30), color = '#5D646F'),
+        plot.background = element_rect(fill = '#EFF2F4'),
+        plot.margin = unit(c(2, 2, 2, 2), 'cm'))
+
+dev.off()
